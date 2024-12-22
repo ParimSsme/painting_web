a98fa38b7dee07b8b2fe85ae85ba0cea8a14a366
Parima <parima.ssme@gmail.com>
2024-12-22 20:28:44 +0430

    add painting screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,408 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
+import 'dart:ui';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class RPSCustomPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path_0 = Path();
+    path_0.moveTo(size.width * 0.9909618, size.height * 0.01844159);
+    path_0.cubicTo(size.width * 0.9909618, size.height * 0.008256599,
+        size.width * 1.081350, 0, size.width * 1.192850, 0);
+    path_0.cubicTo(
+        size.width * 1.304349,
+        0,
+        size.width * 1.394737,
+        size.height * 0.008256599,
+        size.width * 1.394737,
+        size.height * 0.01844159);
+    path_0.lineTo(size.width * 1.394737, size.height * 0.9959820);
+    path_0.cubicTo(
+        size.width * 1.394737,
+        size.height * 1.006166,
+        size.width * 1.304349,
+        size.height * 1.014423,
+        size.width * 1.192850,
+        size.height * 1.014423);
+    path_0.cubicTo(
+        size.width * 1.081350,
+        size.height * 1.014423,
+        size.width * 0.9909618,
+        size.height * 1.006166,
+        size.width * 0.9909618,
+        size.height * 0.9959820);
+    path_0.lineTo(size.width * 0.9909618, size.height * 0.6239507);
+    path_0.cubicTo(
+        size.width * 0.9909618,
+        size.height * 0.6072188,
+        size.width * 0.9042803,
+        size.height * 0.5914748,
+        size.width * 0.7572368,
+        size.height * 0.5814976);
+    path_0.lineTo(size.width * 0.3389882, size.height * 0.5531178);
+    path_0.cubicTo(
+        size.width * 0.1919447,
+        size.height * 0.5431406,
+        size.width * 0.1052632,
+        size.height * 0.5273966,
+        size.width * 0.1052632,
+        size.height * 0.5106647);
+    path_0.lineTo(size.width * 0.1052632, size.height * 0.1564026);
+    path_0.cubicTo(
+        size.width * 0.1052632,
+        size.height * 0.1396707,
+        size.width * 0.1919447,
+        size.height * 0.1239267,
+        size.width * 0.3389882,
+        size.height * 0.1139490);
+    path_0.lineTo(size.width * 0.7859632, size.height * 0.08362103);
+    path_0.cubicTo(
+        size.width * 0.9149342,
+        size.height * 0.07487019,
+        size.width * 0.9909618,
+        size.height * 0.06106034,
+        size.width * 0.9909618,
+        size.height * 0.04638486);
+    path_0.lineTo(size.width * 0.9909618, size.height * 0.01844159);
+    path_0.close();
+
+    Paint paint_0_fill = Paint()..style = PaintingStyle.fill;
+    paint_0_fill.color = Color(0xff383A57);
+    canvas.drawPath(path_0, paint_0_fill);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+void main() {
+  runApp(const PaintingApp());
+}
+
+class PaintingApp extends StatelessWidget {
+  const PaintingApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const DrawingScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+enum Tool { brush, pencil, fill }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class DrawingScreen extends StatefulWidget {
+  const DrawingScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _DrawingScreenState createState() => _DrawingScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _DrawingScreenState extends State<DrawingScreen> {
+  List<DrawingPoint?> points = [];
+  Color selectedColor = Colors.black;
+  double strokeWidth = 4.0;
+  Tool selectedTool = Tool.brush;
+  Color backgroundColor = Colors.white;
 
-  void _incrementCounter() {
+  void _handlePanStart(Offset position) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (selectedTool == Tool.fill) {
+        backgroundColor = selectedColor; // Fill the canvas with selected color
+      } else {
+        points.add(
+          DrawingPoint(
+            position: position,
+            paint: Paint()
+              ..color = selectedColor
+              ..isAntiAlias = true
+              ..strokeWidth = strokeWidth
+              ..strokeCap = StrokeCap.round,
+          ),
+        );
+      }
+    });
+  }
+
+  void _handlePanUpdate(Offset position) {
+    if (selectedTool != Tool.fill) {
+      setState(() {
+        points.add(
+          DrawingPoint(
+            position: position,
+            paint: Paint()
+              ..color = selectedColor
+              ..isAntiAlias = true
+              ..strokeWidth = strokeWidth
+              ..strokeCap = StrokeCap.round,
+          ),
+        );
+      });
+    }
+  }
+
+  void _handlePanEnd() {
+    setState(() {
+      points.add(null); // Add null to separate strokes
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: backgroundColor,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Painting App'),
+        backgroundColor: Colors.blueAccent,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () {
+              setState(() {
+                points.clear();
+                backgroundColor = Colors.white;
+              });
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Row(
+        children: [
+          SizedBox(
+            width: 200,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                DecoratedBox(
+                  decoration: BoxDecoration(color: Color(0xff9295c5)),
+                  child: CustomPaint(
+                    size: Size(70, double.infinity),
+                    painter: RPSCustomPainter(),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 110.0, left: 5),
+                      child: Column(
+                        spacing: 15,
+                        children: [
+                          _buildColorCircle(Color(0xff61c554)),
+                          _buildColorCircle(Color(0xffed695e)),
+                          _buildColorCircle(Color(0xfff4bf4f)),
+                          _buildColorCircle(Color(0xff4d8bb7)),
+                          ElevatedButton(
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                  title: Text('Select Color'),
+                                  content: SingleChildScrollView(
+                                    child: Column(
+                                      children: Colors.primaries.map((color) {
+                                        return GestureDetector(
+                                          onTap: () {
+                                            setState(() {
+                                              selectedColor = color;
+                                            });
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Container(
+                                            width: double.infinity,
+                                            height: 50,
+                                            color: color,
+                                          ),
+                                        );
+                                      }).toList(),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                                shape: CircleBorder(),
+                                padding: EdgeInsets.all(10),
+                                backgroundColor: Color(0x33000000)),
+                            child: const Icon(Icons.add, color: Colors.white),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(color: Color(0xff9295c5)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Tools'),
+                        Divider(),
+                        Row(
+                          children: [
+                            _buildToolButton(Tool.pencil, 'pen'),
+                            _buildToolButton(Tool.brush, 'pen_fancy'),
+                            // _buildToolButton(Tool.fill, Icons.format_color_fill),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            _buildToolButton(Tool.fill, 'fill'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Expanded(
+            child: GestureDetector(
+              onPanStart: (details) => _handlePanStart(details.localPosition),
+              onPanUpdate: (details) => _handlePanUpdate(details.localPosition),
+              onPanEnd: (details) => _handlePanEnd(),
+              child: CustomPaint(
+                painter: DrawingPainter(
+                    points: points, backgroundColor: backgroundColor),
+                size: Size.infinite,
+              ),
             ),
-          ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: _buildBottomToolbar(),
+    );
+  }
+
+  Widget _buildBottomToolbar() {
+    return Container(
+      color: Colors.blueAccent,
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // Tool Selector
+          Row(
+            children: [
+
+            ],
+          ),
+          // Color Picker
+          Row(
+            children: [
+              _buildColorCircle(Colors.black),
+              _buildColorCircle(Colors.red),
+              _buildColorCircle(Colors.green),
+              _buildColorCircle(Colors.blue),
+              _buildColorCircle(Colors.yellow),
+            ],
+          ),
+          // Stroke Width Selector
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    strokeWidth = strokeWidth > 1 ? strokeWidth - 1 : 1;
+                  });
+                },
+                icon: const Icon(Icons.remove, color: Colors.white),
+              ),
+              Text(
+                strokeWidth.toInt().toString(),
+                style: const TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    strokeWidth++;
+                  });
+                },
+                icon: const Icon(Icons.add, color: Colors.white),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildToolButton(Tool tool, String icon) {
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          selectedTool = tool;
+          strokeWidth = tool == Tool.brush
+              ? 8.0
+              : 2.0; // Adjust stroke width for brush/pencil
+        });
+      },
+      style: ElevatedButton.styleFrom(
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(10),
+          backgroundColor: selectedTool == tool ? Colors.white : Colors.white70,),
+      child: SvgPicture.asset('assets/$icon.svg'),
+    );
+  }
+
+  Widget _buildColorCircle(Color color) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedColor = color;
+        });
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 4),
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+          border: selectedColor == color
+              ? Border.all(color: Colors.white, width: 2)
+              : null,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class DrawingPoint {
+  final Offset position;
+  final Paint paint;
+
+  DrawingPoint({required this.position, required this.paint});
+}
+
+class DrawingPainter extends CustomPainter {
+  final List<DrawingPoint?> points;
+  final Color backgroundColor;
+
+  DrawingPainter({required this.points, required this.backgroundColor});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final backgroundPaint = Paint()..color = backgroundColor;
+    canvas.drawRect(Offset.zero & size, backgroundPaint);
+
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(
+            points[i]!.position, points[i + 1]!.position, points[i]!.paint);
+      } else if (points[i] != null && points[i + 1] == null) {
+        canvas.drawCircle(points[i]!.position, points[i]!.paint.strokeWidth / 2,
+            points[i]!.paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
